8e2acde8af739593c58c620ca0e23e0a36b5a019
Daniel Braimah <dannysky10@users.noreply.github.com>
2015-07-27 19:33:43 -0400

    Update script2.js

--- UnlockDaniel/JavaScript:jQuery/script2.js
+++ UnlockDaniel/JavaScript:jQuery/script2.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
     
+     /*Assigning the id's to the $variables */
     $aboutMeHeader = $("#header");
     $indexHeader = $("#indexHeader");
     $fadeLayout = $("#fadeLayout");
     $fadeH2 = $("#header2Fade");
     $tvHeader = $("#fadeTV");
-   /*$contactHeader = $(".contactHeader");*/
+   
+       /* Using the .show method for animating the headers*/
     $tvHeader.show(1600);
     $indexHeader.show(1600);
     $fadeLayout.show(1600);
     $aboutMeHeader.show(1600);
     $fadeH2.show(1600);
 
-    /*$contactHeader.show(1600);*/
+
 
 
  });
